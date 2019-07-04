5f7d832a1e971558b498ace4de59bf60ead500f2
ANDYLLY <52456074+ANDYLLY@users.noreply.github.com>
2019-07-04 09:41:18 +0800

    Add files via upload

---
+++ CAR_TWO_WHEEL/CAR_TWO_WHEEL.ino
@@ -0,0 +1,65 @@
+
+void setup() 
+
+{
+
+  pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+Serial.begin(9600);
+
+}
+
+char dame;
+
+void loop()
+
+{
+  if(Serial.available()>0)
+    {
+      dame=Serial.read();
+      switch(dame)
+      {
+        case'F':forward();break;
+        case'B':backward();break;
+        case'L':leftward();break;
+        case'R':rightward();break;
+        default:
+        digitalWrite(8,LOW);
+      digitalWrite(9,LOW);
+      digitalWrite(11,LOW);
+      digitalWrite(12,LOW);
+      }
+    }
+
+}
+void forward()
+{
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+  
+}
+void backward()
+{
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(12,HIGH);
+}
+void rightward()
+{
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(12,HIGH);
+}
+void leftward()
+{
+  digitalWrite(8,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+}
