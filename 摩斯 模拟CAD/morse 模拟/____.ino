a71dcbc3904597a1b5ea88deb30a35d3fb100647
ANDYLLY <52456074+ANDYLLY@users.noreply.github.com>
2019-07-05 10:26:25 +0800

    morse display

---
+++ 摩斯 模拟CAD/morse 模拟/____.ino
@@ -0,0 +1,124 @@
+#ifndef _MORSE_H
+#define _MORSE_H
+class Morse
+{
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+  private:
+    int _pin;
+};
+#endif /*_MORSE_H*/
+
+Morse::Morse(int pin)
+{
+  pinMode(pin,OUTPUT);
+  _pin=pin;
+}
+
+void Morse::dot()
+{
+  digitalWrite(_pin,HIGH);
+  delay(250);
+  digitalWrite(_pin,LOW);
+  delay(250);
+}
+
+void Morse::dash()
+{
+  digitalWrite(_pin,HIGH);
+  delay(1000);
+  digitalWrite(_pin,LOW);
+  delay(250);
+}
+
+Morse morse(13);//13号引脚
+
+void setup() 
+
+{
+
+  Serial.begin(9600);//波特率
+
+
+
+}
+
+
+
+void loop()
+
+{
+
+  char a=Serial.read();
+
+  switch(a)
+
+  {
+
+    case'a':morse.dot();morse.dash();break;
+
+    case'b':morse.dash();morse.dot();morse.dot();morse.dot();break;
+
+    case'c':morse.dash();morse.dot();morse.dash();morse.dot();break;
+
+    case'd':morse.dash();morse.dot();morse.dot();break;
+
+    case'e':morse.dot();break;
+
+    case'f':morse.dot();morse.dot();morse.dash();morse.dot();break;
+
+    case'g':morse.dash();morse.dash();morse.dot();break;
+
+    case'h':morse.dot();morse.dot();morse.dot();morse.dot();break;
+
+    case'i':morse.dot();morse.dot();break;
+
+    case'j':morse.dot();morse.dash();morse.dash();morse.dash();break;
+
+    case'k':morse.dash();morse.dot();morse.dash();break;
+
+    case'l':morse.dot();morse.dash();morse.dot();morse.dot();break;
+
+    case'm':morse.dash();morse.dash();break;
+
+    case'n':morse.dash();morse.dot();break;
+
+    case'o':morse.dash();morse.dash();morse.dash();break;
+
+    case'p':morse.dot();morse.dash();morse.dash();morse.dot();break;
+
+    case'q':morse.dash();morse.dash();morse.dot();morse.dash();break;
+
+    case'r':morse.dot();morse.dash();morse.dot();break;
+
+    case's':morse.dot();morse.dot();morse.dot();break;
+
+    case't':morse.dash();break;
+
+    case'u':morse.dot();morse.dot();morse.dash();break;
+
+    case'v':morse.dot();morse.dot();morse.dot();morse.dash();break;
+
+    case'w':morse.dot();morse.dash();morse.dash();break;
+
+    case'x':morse.dash();morse.dot();morse.dot();morse.dash();break;
+
+    case'y':morse.dash();morse.dot();morse.dash();morse.dash();break;
+
+    case'z':morse.dash();morse.dash();morse.dot();morse.dot();break;
+
+    case'\n':morse.dot();morse.dot();morse.dot();morse.dot();morse.dot();morse.dot();break;//换行代表******
+
+    case' ':morse.dash();morse.dash();morse.dash();morse.dash();morse.dash();morse.dash();break;//空格代表------
+
+  
+
+  }
+
+  delay(750);//三个小点
+
+
+
+}
